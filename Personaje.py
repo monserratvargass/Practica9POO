67d008719b40aa486886caf4f97f53156b2c2ee4
monserratvargass <guvamonse@gmail.com>
2023-02-20 15:29:37 -0600

    definir metodo correr

--- Personaje.py
+++ Personaje.py
@@ -3,3 +3,10 @@ class Personaje:
     especie="Humano"
     nombre="Masterchief"
     altura=2.70
+
+    #metodos
+    def correr(self, status):
+        if(status):
+            print("El personaje"+self.nombre+"esta corriendo")
+        else:
+            print("El personaje"+self.nombre+"se detuvo")
