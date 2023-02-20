c7df3ff1200c3ca1a2752162b0161a455e4c655a
monserratvargass <guvamonse@gmail.com>
2023-02-20 15:41:55 -0600

    Creamos la instancia

---
+++ Objetos.py
@@ -0,0 +1,9 @@
+from.Personaje import*
+
+#Creamos una instancia de la clase personaje, en este caso la instancia se llama heroe.
+Heroe=Personaje()
+
+#Usamos los atributos
+print("El personaje se llama"+ Heroe.nombre)
+print("Pertenece a la especie"+ Heroe.especie)
+print("Y una altura de:"+ Heroe.altura)
