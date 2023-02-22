66690ea51426a199d42213245f882a17a043123f
monserratvargass <guvamonse@gmail.com>
2023-02-22 15:13:53 -0600

    Solicitud y guardar de datos del heroe y del villano

--- Objetos.py
+++ Objetos.py
@@ -1,4 +1,17 @@
 from Personaje import *
+#Solicitamos
+print("")
+print("### Solicitud datos heroe ###")
+especieH=input("Escribe la especie del heroe:")
+nombreH=input("Escribe el nombre del heroe:")
+alturaH=float(input("Escribe la altura del heroe:"))
+recargaH=int(input("Ingresa las balas para el heroe"))
+
+print("### Solicitud datos villanos ###")
+especieV=input("Escribe la especie del villano:")
+nombreV=input("Escribe el nombre del villano:")
+alturaV=float(input("Escribe la altura del villano:"))
+recargaV=int(input("Ingresa las balas para el villano"))
 
 #Creamos una instancia de la clase personaje, en este caso la instancia se llama heroe.
 Heroe=Personaje()
