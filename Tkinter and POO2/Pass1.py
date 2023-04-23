90b0d7e922e42147d5a80769fa9bae80442247b1
monserratvargass <guvamonse@gmail.com>
2023-04-23 02:14:04 -0600

    Practica 13 (Generador de contraseña)

--- Tkinter and POO2/Pass1.py
+++ Tkinter and POO2/Pass1.py
@@ -1,31 +1,11 @@
 from tkinter import messagebox
 
 class Pass1:
-    def __init__(self,contra):
-        self.__contraseña=contra
+    def __init__(self):
+        self.__contraseña=8
 
-    def validarcontraseña(contra):
-        numeros=0
-        mayusculas=0
-        minusculas=0
-
-        for caracteres in contra:
-            if caracteres.isspace():
-                messagebox.showerror("NO PERMITE CONTRASEÑAS CON ESPACIO")
-                return False
-            elif caracteres.isdigit():
-                messagebox.showinfo("Contiene al menos 4 numeros")
-                numeros +=1
-            elif caracteres.isupper():
-                messagebox.showinfo("Contiene al menos una mayuscula")
-                mayusculas +=1
-            elif caracteres.islower("Contiene al menos una minuscula"):
-                minusculas +=1
-
-        return numeros>4 and mayusculas !=0 and minusculas >=4
-
-    def getEspecie(self):
-        return self.__contraseña
-    
-    def setEspecie(self,contra):
-        self.__contraseña= contra
+    def validarcontraseña(self, contra):
+        if contra>8:
+            self.__contraseña=contra
+        else:
+            self.__contraseña
--- Tkinter and POO2/Pass2.py
+++ Tkinter and POO2/Pass2.py
@@ -1,32 +1,84 @@
 from tkinter import *
 from Pass1 import *
+import random
+import string
 import tkinter as tk
 
-#password=Pass1(contr)
+password=Pass1()
 
-#def ejecutaVal():
-    #password.validarcontraseña(Pass1.get())
+def ejecutaGen():
+    txtco.delete(0, tk.END)
+    txtco.insert(0, ejecutaGen)
+
+    num=int(longitud.get())
+    password.validarcontraseña(num)
+
+    caracteres=string.punctuation
+    mayusculas=string.ascii_uppercase
+    letras=string.ascii_letters
+
+    contraseña=''
+
+    especiales= messagebox.askyesno('Caracteres especiales','¿Desea usar caracteres especiales en su contraseña?')
+    mayus=messagebox.askyesno('Mayusculas','¿Desea usar mayusuclas en su contraseña?')
+
+    if especiales==True and mayus==True:
+        alfabeto=caracteres + letras
+        for i in range(num):
+            contraseña+=''.join(random.choice(alfabeto))
+            print(contraseña)
+        return contraseña
+    if especiales==False and mayus==True:
+        alfabeto=letras
+        for i in range(num):
+            contraseña+=''.join(random.choice(alfabeto))
+        return contraseña
+    if especiales==True and mayus==False:
+        alfabeto=caracteres + mayusculas
+        for i in range(num):
+            contraseña+''.join(random.choice(alfabeto))
+        return contraseña
+    if especiales==False and mayus==False:
+        alfabeto=mayusculas
+        for i in range(num):
+            contraseña+=''.join(random.choice(alfabeto))
+        return contraseña
+
 
 ventana=Tk()
-ventana.title("Password")
+ventana.title("Generador de contraseñas")
 ventana.geometry("300x150")
 
-password1=Label(ventana, text="Ingrese cantidad de caracteres:")
-password1.pack()
+longitud=tk.StringVar()
+lbllongitud=Label(ventana,text="Indique longitud de la contraseña: ")
+lbllongitud.pack()
+txtlongitud=Entry(ventana, textvariable=longitud)
+txtlongitud.pack()
 
-#entry_pass1=tk.StringVar()
-pa=Entry(ventana, width=40, textvariable=Pass1)
-pa.pack()
-paEntry= pa.get()
+lblco=Label(ventana,text='Contraseña generada')
+lblco.pack()
+txtco=Entry(ventana)
+txtco.pack()
 
-password3=Label(ventana, text="")
-password3.pack()
+'''minus=tk.StringVar()
+lblminus=Label(ventana,text="Indique numero minimo de minusculas: ")
+lblminus.pack()
+txtminus=Entry(ventana, textvariable=minus)
+txtminus.pack()
 
-password2=Entry(ventana,width=40,textvariable=Pass1)
-password2.pack()
+mayus=tk.StringVar()
+lblmayus=Label(ventana,text="Indique numero minimo de mayus: ")
+lblmayus.pack()
+txtmayus=Entry(ventana, textvariable=mayus)
+txtmayus.pack()
 
+num=tk.StringVar()
+lblnum=Label(ventana,text="Indique numero minimo de caracteres numericos: ")
+lblnum.pack()
+txtnum=Entry(ventana, textvariable=num)
+txtnum.pack()'''
 
-#botonValidar=Button(ventana,text="Validar",command=ejecutaVal())
-#botonValidar.pack()
+botonGenerar=Button(ventana,text="Generar contraseña", command=ejecutaGen)
+botonGenerar.pack()
 
 ventana.mainloop()
